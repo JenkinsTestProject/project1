a712a04fd9e118bc7b4089c38581cfcc1aa21af3
Anshuma <ansh783@gmail.com>
2015-06-23 00:34:22 +0530

    First commit

---
+++ src/main/java/test/HelloWorld.java
@@ -0,0 +1,14 @@
+
+package test;
+
+/**
+ * @author a
+ * 
+ */
+public class HelloWorld {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World - A test project for Jenkins");
+    }
+
+}
---
+++ src/test/java/test/HelloWorldTest.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+
+package test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * @author a
+ * 
+ */
+public class HelloWorldTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {}
+
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void secondTest() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void thirdTest() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void fourthTest() {
+        Assert.assertTrue(true);
+    }
+
+}
